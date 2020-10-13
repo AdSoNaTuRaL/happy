686705fdd5d3145e0ba10d3afb8a50edff3cd95e
adsonatural <adsonhenriquesilva@gmail.com>
2020-10-13 14:41:20 +0200

    Add new column

--- backend/src/database/migrations/1602589612358-CreateOrphanages.ts
+++ backend/src/database/migrations/1602589612358-CreateOrphanages.ts
@@ -39,6 +39,10 @@ export default class CreateOrphanages1602589612358 implements MigrationInterface
             type: 'text',
           },
           {
+            name: 'opening_hours',
+            type: 'varchar',
+          },
+          {
             name: 'open_on_weekends',
             type: 'boolean',
             default: false,
