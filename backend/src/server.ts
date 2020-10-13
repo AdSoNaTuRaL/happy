19995cbac3cabcf7a43925c7d25e76fceefd57e0
adsonatural <adsonhenriquesilva@gmail.com>
2020-10-13 15:34:03 +0200

    Refactoring server.ts for use handle errors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import 'express-async-errors';
 import path from 'path';
 
 import './database/connection';
+import errorHandler from './errors/handler';
 
 import routes from './routes';
 
@@ -10,5 +12,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 app.listen(3333);
