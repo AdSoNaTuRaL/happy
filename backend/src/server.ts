890d613f8fa4d00a32011cf345650101edc864b7
adsonatural <adsonhenriquesilva@gmail.com>
2020-10-13 15:48:06 +0200

    Updating server.ts for use cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import path from 'path';
 
@@ -9,6 +10,7 @@ import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
