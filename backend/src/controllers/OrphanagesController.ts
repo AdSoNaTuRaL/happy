21a315a333177622844f74460423878f6c660a98
adsonatural <adsonhenriquesilva@gmail.com>
2020-10-13 15:09:50 +0200

    Refactoring code for work with images

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -34,6 +34,11 @@ export default {
         } = request.body;
     
         const orphanagesRepository = getRepository(Orphanage);
+
+        const requestImages =  request.files as Express.Multer.File[];
+        const images = requestImages.map(image => {
+            return { path: image.filename}
+        })
     
         const orphanage = orphanagesRepository.create({
             name,
@@ -42,7 +47,8 @@ export default {
             about, 
             instructions, 
             opening_hours, 
-            open_on_weekends 
+            open_on_weekends,
+            images
         });
     
         await orphanagesRepository.save(orphanage);
