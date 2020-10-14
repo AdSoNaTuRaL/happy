e8bc8564af78d51ec6c37d4d539265d9bfdf34b2
adsonatural <adsonhenriquesilva@gmail.com>
2020-10-14 16:05:58 +0200

    Changing folder name

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { FiPlus } from 'react-icons/fi';
+import { FiArrowRight, FiPlus } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
-import mapMarkerImg from '../images/map-mark.svg';
+import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
+import mapIcon from '../utils/mapIcon';
 
 function OrphanagesMap() {
     return (
@@ -32,10 +33,24 @@ function OrphanagesMap() {
                 style={{ width: '100%', height: '100%' }}
             >
                 {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
-                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+                <TileLayer 
+                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
+                />
+
+                <Marker
+                    icon={mapIcon}
+                    position={[37.7797221,-3.7943167]}
+                >
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup" >
+                        Lar das meninas
+                        <Link to="/orphanages/1">
+                            <FiArrowRight size={20} color="#FFF" />
+                        </Link>
+                    </Popup>
+                </Marker>
             </Map>
 
-            <Link className="create-orphanage" to="">
+            <Link className="create-orphanage" to="/orphanages/create">
                 <FiPlus size={32} color="#fff" />
             </Link>
         </div>
