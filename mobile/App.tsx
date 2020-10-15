5be5a18585b6089e09a41b813fecdcdf56d32b27
adsonatural <adsonhenriquesilva@gmail.com>
2020-10-15 13:51:45 +0200

    First commit mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
